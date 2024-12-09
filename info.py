726112b557b75303e18616986f663ad9828d7fdd
Itzmecp <121564783+Cpmodzyt@users.noreply.github.com>
2024-12-09 16:42:18 +0530

    Update info.py

--- info.py
+++ info.py
@@ -74,7 +74,7 @@ COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Files')
 SUPPORT_LINK = environ.get('SUPPORT_LINK', 'https://t.me/Interworld_Request')
 OWNER_USERNAME = environ.get("OWNER_USERNAME", "https://t.me/Itzmecp")
 UPDATES_LINK = environ.get('UPDATES_LINK', 'https://t.me/Interworldmx')
-FILMS_LINK = environ.get('FILMS_LINK', 'https://t.me/Interworldmx')
+FILMS_LINK = environ.get('FILMS_LINK', 'https://t.me/Interworld_Request')
 TUTORIAL = environ.get("TUTORIAL", "https://t.me/Interworldmx")
 VERIFY_TUTORIAL = environ.get("VERIFY_TUTORIAL", "https://t.me/Interworldmx")
 
