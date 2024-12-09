e22687d701f6a542fc1c73468467349e3aec2674
Itzmecp <121564783+Cpmodzyt@users.noreply.github.com>
2024-12-09 16:15:24 +0530

    Update info.py

--- info.py
+++ info.py
@@ -63,7 +63,7 @@ else:
     SUPPORT_GROUP = int(SUPPORT_GROUP)
 
 # MongoDB information
-DATABASE_URL = environ.get('DATABASE_URL', "mongodb+srv://AnimeFlix:Itzmecp@cluster0.qxdxy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DATABASE_URL = environ.get('DATABASE_URL', "mongodb+srv://itzcp:itzmecp@cluster0.rrphufd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 if len(DATABASE_URL) == 0:
     print('Error - DATABASE_URL is missing, exiting now')
     exit()
